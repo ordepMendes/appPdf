8bfe76407363681fef65667b5fba24ae0d262251
ordepMendes <pedrohh0012@hotmail.com>
2024-04-02 22:36:40 -0300

    feat:

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Alert, Button, StyleSheet, Text, View } from "react-native";
+import * as DocumentPicker from "expo-document-picker";
+import { useState } from "react";
 
 export default function App() {
+  const [selectedPdf, setSelectedPdf] = useState();
+
+  async function pickDocument() {
+    let result = await DocumentPicker.getDocumentAsync({
+      type: "application/pdf",
+      copyToCacheDirectory: true,
+    });
+
+    if(result.assets && result.assets[0].uri){
+      console.log(result.assets[0].uri);
+      setSelectedPdf(result.assets[0].uri);
+    }else{
+      Alert.alert('ATENÇÃO', 'Nenhum pdf selecionado')
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View
+        style={{
+          flexDirection: "row",
+          gap: 14,
+          marginTop: 14,
+          marginBottom: 14,
+        }}
+      >
+        <Button title="Escolher PDF" onPress={pickDocument} />
+        <Button title="Fechar PDF" />
+      </View>
     </View>
   );
 }
@@ -13,8 +40,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
